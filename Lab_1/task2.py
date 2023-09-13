b936b35f18effca99e3b74358fd1dad759c55b93
GeraDot4815 <86369642+GeraDot4815@users.noreply.github.com>
2023-09-13 23:32:31 +0300

    Update task2.py

--- Lab_1/task2.py
+++ Lab_1/task2.py
@@ -1,52 +1,79 @@
 import csv
-class Anket: #Класс анкетируемого, где хранятся его ответы и имя
-    def __init__(self, answers, name):
-        self.answers=answers
-        self.name=name
-def ParseAnswers(): #Создаем список с объектами класса Anket
-    Ankets=[]
-    with open("opros.csv", encoding="utf-8") as file:
-        reader=csv.DictReader(file, delimiter=",")
-        for row in reader:
-            row.pop("time")
-            name=row["Имя Фамилия"]
-            row.pop("Имя Фамилия")
-            answers=list(row.values())
-            anket=Anket(answers, name)
-            Ankets.append(anket)
-    return Ankets
-def GetQuestions(): #Достаем из файла вопросы
-    Questions=[]
-    with open("opros.csv", encoding="utf-8") as file:
-        reader=csv.DictReader(file, delimiter=",")
-        for row in reader: #Не придумал, как получить заглавную строку с ключами, поэтому такой костыль
-            row.pop("time")
-            row.pop("Имя Фамилия")
-            Questions=list(row.keys())
-            break
-    return Questions
 
-#def CheckAnket(length: int, answers_form: list[str], ankets_form:)
-def StartDialog(Questions, Ankets: list[Anket]): #Ответами пользователя заполняем новый список
-    NewForm = []
-    for quest in Questions:
-        print(quest)
+
+class Anket:  # Класс анкетируемого, где хранятся его ответы и имя
+    def __init__(self, answers, name):
+        self.answers = answers
+        self.name = name
+
+
+def parse_answers() -> list[Anket]:  # Создаем список с объектами класса Anket
+    allankets = []
+
+    with open("opros.csv", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",")
+
+        for row in reader:
+            row.pop("time")  # Этот столбец нам не нужен
+
+            name = row["Имя Фамилия"]  # Получаем имя опрашиваемого
+            row.pop("Имя Фамилия")  # Теперь этот столбец нам тоже не нужен
+
+            answers = list(row.values())  # Получаем ответы на все вопросы в виде списка
+
+            anket = Anket(answers, name)  # Создаем экземпляр класса и сохраняем в список
+            allankets.append(anket)
+
+    return allankets
+
+
+def get_questions() -> list[str]:  # Достаем из файла вопросы
+    questions = []
+
+    with open("opros.csv", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",")
+
+        for row in reader:  # Не придумал, как получить заглавную строку с ключами, поэтому такой костыль
+            row.pop("time")  # Удаляем ненужные столбцы
+            row.pop("Имя Фамилия")
+
+            questions = list(row.keys())  # Получаем все вопросы
+            break  # Более одной итерации делать ненужно
+    return questions
+
+
+def check_anket(length: int, answers_form: list[str], anket_form: Anket) -> bool:
+    for i in range(length):
+        if answers_form[i] != anket_form.answers[i]:
+            return False
+    return True
+
+
+def start_dialog(questions: list[str], ankets: list[Anket]):  # Ответами пользователя заполняем новый список
+    newform = []
+    flag = False
+    for i, question in enumerate(questions):
+        # Создаем список из анкет, в которых введенные ответы совпадают
+        #Для оптимизации эти команды должны выполняться раньше, чем будет задан вопрос
+        if i!=0:
+                sameankets = [anket for anket in ankets if check_anket(i, newform, anket)]
+                if len(sameankets) == 1: #Если существует только одна такая анкета, определяем этого человека
+                    print(f"Вы - {sameankets[0].name}")
+                    flag = True
+                    break
+        #Задаем вопрос
+        print(question)
         ans = input("Type y/n: ")
         ans = ans.lower()
         if ans == 'y':
-            NewForm.append("Да")
+            newform.append("Да")
         else:
-            NewForm.append("Нет")
-        #same_ankets=[i for i in Ankets if che]
-    flag = False
-    for anket in Ankets: #Сравниваем ответы с существующими объектами
-        if NewForm == anket.answers:
-            print(f"Вы - {anket.name}")
-            flag = True
-            break
-    if not flag: print("Простите, ваши ответы никому не соответствуют.")
+            newform.append("Нет")
+
+    if not flag: print("Простите, мы не можем определить, кто вы.")
+
 
 if __name__ == "__main__":
-    Ankets=ParseAnswers()
-    Questions=GetQuestions()
-    StartDialog(Questions, Ankets)
+    ankets = parse_answers()
+    questions = get_questions()
+    start_dialog(questions, ankets)
